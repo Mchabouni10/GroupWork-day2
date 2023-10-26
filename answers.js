209ffb840bbcba560a4fb2ae30b0c7931218078d
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-10-26 13:23:59 -0500

    Find the Median answered

--- answers.js
+++ answers.js
@@ -119,7 +119,73 @@ of the array rather than counting it and hard coding it) - Thought question: did
 
 // console.log(halfMovies.indexOf('Fast and Furious'))
 
+//Where is Waldo
 
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+// ["Lucinda", "Jacc", "Neff", "Snoop"],
+// ["Petunia", ["Baked Goods", "Waldo"]]];
+
+//Remove Eggbert (hint look at the slice/splice method(s))
+// whereIsWaldo.splice(1, 1)
+// console.log(whereIsWaldo)
+
+//Change "Neff" to "No One"
+
+// console.log(whereIsWaldo.indexOf("Neff"))
+// console.log(whereIsWaldo[1][2])
+
+// whereIsWaldo[1][2] = "No One"
+// console.log(whereIsWaldo[1][2])
+
+//Access and console.log "Waldo"
+
+// console.log(whereIsWaldo[2][1][1])
+
+//===================================================================
+
+//Excited Kitten
+
+//Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+
+// for(let i = 1; i <= 20; i++){
+//     console.log("Love me, pet me! HSSSSSS!")
+// }
+
+//For every even number in your loop, log
+//"...human...why you taking pictures of me?...", "...the catnip made me do it...", 
+//or "...why does the red dot always get away..." at random.
+
+// let messages = [
+//     "...human...why you taking pictures of me?...", 
+//     "...the catnip made me do it...", 
+//     "...why does the red dot always get away..."
+// ]
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(messages[Math.floor(Math.random() * messages.length)])
+//     }
+//     else{
+//         console.log(i + " Love me, pet me! HSSSSSS!")
+//     }
+// }
+
+//==============================================================================
+
+//Find the Median
+
+//Find the median number in the following nums array, then console.log that number.
+
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+// nums.sort()
+// console.log(nums)
+
+// let median = Math.floor(nums.length / 2)
+
+// console.log(nums[median])
+
+//=========================================================================================
 
 
 
